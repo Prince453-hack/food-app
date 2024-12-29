421fa6b1cc65ab469354960b219f6726a884616d
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-12-29 21:43:21 +0530

    new

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -45,6 +45,8 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
         }}
       />
+
+      {/* for commit */}
     </Tabs>
   );
 }
