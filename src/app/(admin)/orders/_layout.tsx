b318ee9615b4560312b65c580163aafcff2d9659
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2025-01-06 17:15:09 +0530

    UI changes (tab changes)

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -3,10 +3,11 @@ import { Stack } from "expo-router";
 export default function Menu() {
   return (
     <Stack>
-      <Stack.Screen
+      {/* <Stack.Screen
         name="index"
         options={{ title: "Orders", headerShown: false }}
-      />
+      /> */}
+      <Stack.Screen name="list" options={{ headerShown: false }} />
     </Stack>
   );
 }
